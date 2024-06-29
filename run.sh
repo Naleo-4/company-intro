80f5a54075331fc8d4ac4b1616e78295177a990b
Naleo-4 <90037354+Naleo-4@users.noreply.github.com>
2024-06-29 14:07:23 +0700

    push to github

---
+++ run.sh
@@ -0,0 +1,20 @@
+
+
+
+dotnet build         
+
+dotnet watch -project Project 
+
+dotnet watch --project Project   
+
+dotnet publish --project ProjectMVC ProjectMVC.sln -c Release -o out    
+
+dotnet publish ProjectMVC.sln -c Release -o out  
+
+dotnet publish -c Release -r any --self-contained true   
+
+dotnet publish -c Release -r browser --self-contained true
+
+dotnet https://github.com/Naleo-4/MVCsolution/blob/master/ProjectMVC/bin/Release/net6.0/browser/ProjectMVC.dll
+
+dotnet clean 
